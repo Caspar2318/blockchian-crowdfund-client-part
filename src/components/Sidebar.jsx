adbf706b2800aca9f88c329ec6d8d7528dca64ae
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-03-16 11:59:09 +1000

    layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { logo, sun } from "../assets";
+import { navLinks } from "../constants";
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
